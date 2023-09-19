89c5d2b206cb9aace967a0aebb6031b00980203f
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-09-19 09:21:49 -0500

    feat(MasterAcademy): :sparkles: express server running
    Configuration is performed for the respective operation of the express server
    
    EduardoMantillaCampus

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,13 @@
-console.log("MasterAcademy");
+import express from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config("../../");
+let appExpress = express();
+
+appExpress.use(express.json());
+
+let config = JSON.parse(process.env.MY_SERVER)
+appExpress.listen(config, ()=>{
+    console.log(`http://${config.hostname}:${config.port}`)
+})
