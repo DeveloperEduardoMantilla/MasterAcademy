542a3dd2f388fe7a94e4f0c7f7f4b66dc9e66d28
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-09-28 17:31:52 -0500

    feat(MasterAcademy): :sparkles: Settings in middleware validateId
    Settings in middleware validateId
    
    EduardoMantillaCampus

--- server/src/middlewares/validateId.js
+++ server/src/middlewares/validateId.js
@@ -2,7 +2,7 @@ const validateId = (req, res, next) => {
     if(!isNaN(parseInt(req.params.id))){
         next();
     }else{
-        res.send({message:"The id parameter is not a number"})
+        res.status(400).send({status:"Error",message:"The id parameter is not a number"})
         return
     }
     
