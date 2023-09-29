887e5f26d82bcfd51fb95c0fac78555e589e8c06
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-09-29 11:06:17 -0500

    feat(MasterAcademy): :sparkles: Adjustments in the FrontEnd dashboard module
    Adjustments were made to the FrontEnd dashboard module where a better structure of the course modules was worked on.
    
    EduardoMantillaCampus

--- src/components/dashboard/Course.jsx
+++ src/components/dashboard/Course.jsx
@@ -1,22 +1,23 @@
 import {react} from "react"
 import "../../assets/styles/courses/course.css"
 
-export default function Course(){
+export default function Course({title,description,urlImg}){
     return (
         <>
           <section className="course">
+          <div className="capa"  style={{backgroundImage: `url(${urlImg})`}}></div>
             <div className="description">
-              <h1>React</h1>
-              <p>We are working on the design of the dashboard that the student will see.</p>
+              <h1>{title}</h1>
+              <p>{description}</p>
             </div>
-            <div className="details">
-              <div className="category">
+            {/*<div className="details">
+            <div className="category">
                 <h3>Front-End</h3>
               </div>
               <div className="date">
                 <h4>2023-10-15</h4>
               </div>
-            </div>
+            </div>*/}
             
             
           </section>
--- src/components/dashboard/Courses.jsx
+++ src/components/dashboard/Courses.jsx
@@ -1,43 +1,33 @@
-import {react} from "react"
+import {useState, useEffect} from "react"
 import Course from "./Course.jsx"
 import "../../assets/styles/dasboard/courses.css";
 
 export default function Courses(){
+  const [coursesData, setCoursesData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://192.168.128.23:5010/cursos/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setCoursesData(data.courses);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
     return (
         <>
         <section className='courses'>
           <h2><img src="https://lukaszadam.com/assets/downloads/desk-illustration-2.svg"  width={"70px"}/> Courses available </h2>
           <div className="courses-content">
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
+              {Array.isArray(coursesData) ? (
+              coursesData.map((course) => (
+                <Course key={course.id} title={course.folder} description={course.nameCourse} urlImg={course.imagenCourse} />
+              ))
+            ) : (
+              <p>Loading...</p>
+            )}
           </div>
         </section>
         </>
