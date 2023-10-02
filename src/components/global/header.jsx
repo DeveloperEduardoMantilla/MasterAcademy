bbe599d98c5e3bee2b5e2f088825ee32ac44fa80
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-10-02 00:13:55 -0500

    feat(MasterAcademy): :sparkles: Implementing fetch for api consumption from FrontEnd
    The api is implemented in the Header module in order to obtain the data of the user who is logging into the application.
    
    EduardoMantillaCampus

--- src/components/global/header.jsx
+++ src/components/global/header.jsx
@@ -1,7 +1,7 @@
 import Home from "../../pages/Home.jsx";
 import { Link } from "react-router-dom";
+import {useState, useEffect, React} from "react"
 import "../../assets/styles/global/header.css"
-import foto from "../../assets/img/profile.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars} from "@fortawesome/free-solid-svg-icons";
 
@@ -14,32 +14,48 @@ async function exit(){
     }
 }
 
-function saludar(){
+function btnSidebar(){
     const aside = document.getElementById('aside');
 
     aside.classList.toggle('show');
 }
 
 export default function Header(){
+  const [UserData, setUserData] = useState([]);
+  useEffect(() => {
+    let ruta="http://localhost:5010/dashboard/userLogout";
+    fetch(ruta, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data);
+      })
+      .catch((error) => {
+        console.log("Error 23 => " + error);
+      });
+  }, []);
+  
     return(
         <>
         <header>
           <nav className="navbar navbar-expand-sm navbar-dark">
             <div className="container">
               <div className="btnHeader">
-                <button onClick={saludar}><FontAwesomeIcon className="rotate-vert-center" icon={faBars} style={{color: "#2E86C1",}}/></button>
+                <button onClick={btnSidebar}><FontAwesomeIcon className="rotate-vert-center" icon={faBars} style={{color: "#2E86C1",}}/></button>
               </div>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <p className="nav-link">eduardo.5253</p>
+                    <p className="nav-link">{UserData.username}</p>
                   </li>
                   <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle pe-5 me-5" to={<Home/>} id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      <img src="https://cdn.discordapp.com/avatars/606870241720401959/dcc4677ea230feaa46fc8a3810a6d08a.png" />
+                    <a className="nav-link dropdown-toggle pe-5 me-5" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src={UserData.profile} />
                     </a>
                     <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                      <li><a className="dropdown-item" to={<Home/>}>Profile</a></li>
+                      <li><Link className="dropdown-item" to={"/dashboard/profile"}>Profile</Link></li>
                       <li><a className="dropdown-item" onClick={exit}>Exit</a></li>
                     </ul>
                   </li>
